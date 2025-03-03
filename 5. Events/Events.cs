3e014d452e054ba8e7dd3e7bde08683c8cba7f10
Valeriia <139450358+Imachan1@users.noreply.github.com>
2025-03-03 08:43:23 +0100

    Update Events.cs

--- 5. Events/Events.cs
+++ 5. Events/Events.cs
@@ -1,7 +1,9 @@
 using Microsoft.SqlServer.Server;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -64,11 +66,63 @@ namespace Zdarzenia
 		{
 			private const string _passwordFilePath = "userPasswords.txt";
 			public static event Action<string, bool> PasswordVerified;
+
+			static PasswordManager()
+			{
+				if (!File.Exists(_passwordFilePath)) 
+				{
+					File.Create(_passwordFilePath).Dispose();
+				}
+			}
+
+			public static void SavePassword(string username, string password)
+			{
+				if (File.ReadLines(_passwordFilePath).Any(line => line.Split(',')[0]) == username))
+				{
+                    Console.WriteLine($"User {username} exists");
+					return;
+                }
+
+				string hashedPassword = HashPassword(password);
+				File.AppendAllText(_passwordFilePath, $"{username},{hashedPassword}");
+                Console.WriteLine($"User {username} added");
+            }
+
+			private static string HashPassword(string password)
+			{
+				using(var sha256 = SHA256.Create())
+				{
+					var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+					return Convert.ToBase64String(bytes);
+				}
+			}
+
+			private static bool VerifyPassword(string username, string password)
+			{
+				string hashedPassword = HashPassword(password);
+				foreach(var line in File.ReadLines(_passwordFilePath))
+				{
+					var parts = line.Split(',');
+					if (parts[0] == username && parts[1] == hashedPassword)
+					{
+						PasswordVerified?.Invoke(username, true);
+						return true;
+					}
+				}
+				PasswordVerified?.Invoke(username, false);
+				return false;
+			}
 		}
 
 
 		static void Main(string[] args)
 		{
-		}
+			PasswordManager.PasswordVerified += (username, success) => Console.WriteLine($"Log in {username} :{(success? "successful":"fail")}");
+			PasswordManager.SavePassword("admin", "pass");
+
+            Console.WriteLine("\nEnter username:");
+			string username = Console.ReadLine();
+            Console.WriteLine();
+        }
 	}
 }
