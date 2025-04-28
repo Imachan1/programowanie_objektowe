623f16dce3b0a422b41a041e9d980f85827dec4a
Valeriia <139450358+Imachan1@users.noreply.github.com>
2025-04-28 08:25:27 +0200

    Add files via upload

---
+++ 06. Testowanie oprogramowania/testowanie_1/CalculatorApp/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public int Add(int x,int y)
+        {
+            return x+y;
+        }
+
+        public int Divide(int a, int b) 
+        { 
+            if (b==0)
+            {
+                throw new DivideByZeroException("Nie mozna dzielicprzez 0");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ 06. Testowanie oprogramowania/testowanie_1/CalculatorApp/Program.cs
@@ -0,0 +1,6 @@
+﻿using CalculatorApp;
+
+Calculator calculator = new Calculator();
+
+int sum = calculator.Add(3, 5);
+Console.WriteLine($"Wynik dodawania: {sum}");
